41dee69ab0836dc376d2853da87f05f111fdda3d
kraimaim <67071210+kraimaim@users.noreply.github.com>
2021-02-24 17:18:46 -0300

    landing page DaftPunk

---
+++ public/readme.txt
@@ -0,0 +1,4 @@
+El landing festival fue realizado en 3 resoluciones:
+IPhone X: max width: 375px (mobile).
+IPad: max width: 768px (Tablets).
+Desktops: max width: 1200px.
---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+const track = document.querySelector('.track');
+let initialPosition = null;
+let moving = false;
+let transform = 0;
+
+const gestureStart = (e) => {
+    initialPosition = e.pageX;
+    moving = true;
+    const transformMatrix = window.getComputedStyle(track).getPropertyValue('transform');
+    if(transformMatrix !== 'none') {
+        transform = parseInt(transformMatrix.split(',')[4].trim());
+    }
+ }
+
+ const gestureMove = (e) => {
+    if(moving) {
+     const currentPosition = e.pageX;
+    const diff = currentPosition - initialPosition;
+    track.style.transform = `translateX(${transform + diff}px)`;
+ }
+ };
+
+ const gestureEnd = (e) => {
+    moving = false;
+}
+
+if(window.PointerEvent){
+    window.addEventListener('pointerdown',gestureStart);
+
+    window.addEventListener('pointermove', gestureMove);
+
+    window.addEventListener('pointerup', gestureEnd);
+}else {
+    window.addEventListener('mousedown',gestureStart);
+
+    window.addEventListener('mousemove', gestureMove);
+    
+    window.addEventListener('mouseup', gestureEnd);
+    
+    window.addEventListener('touchdown',gestureStart);
+
+    window.addEventListener('touchmove', gestureMove);
+    
+    window.addEventListener('touchup', gestureEnd);
+}
+
+
